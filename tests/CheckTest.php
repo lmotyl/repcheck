851224244605dbd2071250bd9908bd1a41872e0e
Lukasz Motyl <lukasz.motyl@miinto.com>
2021-03-17 13:32:11 +0100

    project init

---
+++ tests/CheckTest.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+
+class CheckTest extends \PHPUnit\Framework\TestCase
+{
+    public function testCheck(): void
+    {
+        $this->assertSame(true, true);
+    }
+
+}
